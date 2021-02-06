f5d8272b8d9b61416c9d51214e3efd3108547fcd
papaponmx <jaime.rios.dev@outlook.com>
2021-02-05 19:26:45 -0600

    Layout wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,55 @@
-import LandingPage from './layout/landingPage';
-import React from 'react';
+import LandingPage from "./layout/landingPage";
+import React from "react";
 
 function App() {
-	return (
-		<LandingPage>
-			<div className='flex flex-col h-full items-center justify-center text-white bg-gradient-to-br from-gray-600 via-teal-700 to-gray-800'>
-				<div className='flex items-center animate-bounce' />
-				<p className='mt-6 tracking-wide'>
-					Edit <code>src/App.jsx</code> and save to reload.
-				</p>
-				<div className='mt-4 flex justify-center'>
-					<a
-						className='ml-4 bg-indigo-500 hover:bg-indigo-600 text-white py-2 px-4 rounded'
-						href='https://tailwindcss.com'
-						target='_blank'
-						rel='noopener noreferrer'
-					>
-						Learn Tailwind
-					</a>
-				</div>
-			</div>
-		</LandingPage>
-	);
+  return (
+    <LandingPage>
+      <section className="font-sans font-thin max-w-full w-screen h-screen text-center text-gray-800 grid grid-cols-2">
+        <div className="flex flex-col justify-center items-center h-screen w-full max-w-full max-h-full">
+          <h1 className="text-8xl max-w-lg mb-14">
+            Describe your product or service
+          </h1>
+
+          <button className="bg-green-300 bg-opacity-30 text-gray-700 rounded-md w-52 py-4">
+            Buy now
+          </button>
+        </div>
+        <div className="img h-screen w-full max-h-full bg-red-100"></div>
+      </section>
+      <section className="bg-pink-100 h-36 flex flex-col justify-center items-center text-4xl font-extralight">
+        <p>El regalo perfecto para ese momento especial</p>
+      </section>
+
+      <section className="font-sans font-thin max-w-full w-screen h-screen text-center text-gray-800 grid grid-cols-2">
+        <div className="img h-screen w-full max-h-full bg-red-100"></div>
+        <div className="flex flex-col justify-center items-center h-screen w-full max-w-full max-h-full">
+          <ul className="list-disc text-left text-xl">
+            <li>
+              Cupidatat aliqua est ullamco do qui commodo culpa consectetur
+              aliquip minim reprehenderit consectetur irure.
+            </li>
+            <li>
+              Ullamco do ullamco consectetur ullamco sunt dolore commodo
+              reprehenderit aliqua magna.
+            </li>
+            <li>
+              Ad proident magna aliqua occaecat magna consectetur nisi excepteur
+              ipsum sit.
+            </li>
+            <li>
+              Veniam et exercitation nostrud exercitation laborum enim anim
+              minim incididunt Lorem culpa culpa amet est.
+            </li>
+            <li>Magna enim cupidatat adipisicing est officia quis.</li>
+          </ul>
+
+          <button className="bg-green-300 bg-opacity-30 text-gray-700 rounded-md w-52 py-4 mt-16">
+            Buy now
+          </button>
+        </div>
+      </section>
+    </LandingPage>
+  );
 }
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-const Header: React.FC<{ cta: string}> = ({ cta }) => {
-    return (
-        <header>
-            TODO: Add nav menu
-            <button className="bg-yellow-300	 text-3xl text-yellow-50 flex justify-center content-center">{cta}</button>
+const Header: React.FC<{ cta: string }> = ({ cta }) => {
+	return (
+		<header>
+			TODO: Add nav menu
+			<button className='bg-yellow-300	 text-3xl text-yellow-50 flex justify-center content-center'>{cta}</button>
+		</header>
+	);
+};
 
-        </header>
-    )
-}
-
-export default Header
-
+export default Header;
