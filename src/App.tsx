4542c3a81a3c582c3993b7e3b7be9832e7a61d23
papaponmx <jaime.rios.dev@outlook.com>
2021-02-07 18:41:15 -0600

    Preview

--- src/App.tsx
+++ src/App.tsx
@@ -14,16 +14,16 @@ function App() {
             Buy now
           </button>
         </div>
-        <div className="img h-screen w-full max-h-full bg-red-100"></div>
+        <div className="img h-screen w-full max-h-full bg-red-100 " />
       </section>
       <section className="bg-pink-100 h-36 flex flex-col justify-center items-center text-4xl font-extralight">
         <p>El regalo perfecto para ese momento especial</p>
       </section>
 
       <section className="font-sans font-thin max-w-full w-screen h-screen text-center text-gray-800 grid grid-cols-2">
-        <div className="img h-screen w-full max-h-full bg-red-100"></div>
+        <div className="img h-screen w-full max-h-full bg-red-100" />
         <div className="flex flex-col justify-center items-center h-screen w-full max-w-full max-h-full">
-          <ul className="list-disc text-left text-xl">
+          <ul className="list-disc text-left text-xl max-w-md border-t-2">
             <li>
               Cupidatat aliqua est ullamco do qui commodo culpa consectetur
               aliquip minim reprehenderit consectetur irure.
@@ -43,7 +43,7 @@ function App() {
             <li>Magna enim cupidatat adipisicing est officia quis.</li>
           </ul>
 
-          <button className="bg-green-300 bg-opacity-30 text-gray-700 rounded-md w-52 py-4 mt-16">
+          <button className="bg-green-300 bg-opacity-30 text-gray-700 rounded-md w-52 py-4 mt-16 border-gray-300 border">
             Buy now
           </button>
         </div>
--- src/layout/landingPage.tsx
+++ src/layout/landingPage.tsx
@@ -1,14 +1,12 @@
-import Header from '../components/Header';
-import React from 'react';
-const LandingPage: React.FC = ({children}) =>  {
-	return (
-        <>
-		<Header cta="Buy now" />
-        {
-            children
-        }
-        </>
-	);
-}
+import Header from "../components/Header";
+import React from "react";
+const LandingPage: React.FC = ({ children }) => {
+  return (
+    <>
+      {/* <Header cta="Buy now" /> */}
+      {children}
+    </>
+  );
+};
 
 export default LandingPage;
